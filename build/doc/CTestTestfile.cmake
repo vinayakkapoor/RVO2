f171a869e460b7712c35100c242699d2239b00ce
Vinayak <vinayakkapoor12@gmail.com>
2023-03-07 16:18:21 +0530

    minor changes to build

--- build/doc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vinayak/draca/RVO2/doc
-# Build directory: /home/vinayak/draca/RVO2/build/doc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/examples/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vinayak/draca/RVO2/examples
-# Build directory: /home/vinayak/draca/RVO2/build/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Roadmap "/home/vinayak/draca/RVO2/build/examples/Roadmap")
-set_tests_properties(Roadmap PROPERTIES  LABELS "medium" TIMEOUT "60" _BACKTRACE_TRIPLES "/home/vinayak/draca/RVO2/examples/CMakeLists.txt;80;add_test;/home/vinayak/draca/RVO2/examples/CMakeLists.txt;0;")
